8c061a018345adc1939e08e610bd75e9009d2cbd
kgkang <kgkang@bemily.com>
2022-04-09 15:26:30 +0900

    scroll add

--- Bookshelf/View/DetailBookViewController.swift
+++ Bookshelf/View/DetailBookViewController.swift
@@ -42,80 +42,76 @@ class DetailBookViewController: UIViewController {
 
     func setupDetailUI() {
 
-//        let scrollView = UIScrollView()
-//        scrollView.backgroundColor = .systemRed
-//        self.view.addSubview(scrollView)
-//
-//        scrollView.anchor(top: view.topAnchor, right: view.trailingAnchor, bottom: view.bottomAnchor, left: view.leadingAnchor)
+        let scrollView = UIScrollView()
+        self.view.addSubview(scrollView)
 
-        let navBarHeight = UIApplication.shared.statusBarFrame.size.height +
-                 (navigationController?.navigationBar.frame.height ?? 0.0)
+        scrollView.anchor(top: view.topAnchor, right: view.trailingAnchor, bottom: view.bottomAnchor, left: view.leadingAnchor)
 
         let detailImage = UIImageView()
         imageLoader.getImage(urlString: book?.image ?? "", completionHandler: { image in
             detailImage.image = image
         })
-        view.addSubview(detailImage)
+        scrollView.addSubview(detailImage)
 
-        detailImage.anchor(top: view.topAnchor, right: nil, bottom: nil, left: nil, centerX: view.centerXAnchor, padding: .init(top: navBarHeight, left: 0, bottom: 0, right: 0))
+        detailImage.anchor(top: scrollView.topAnchor, right: nil, bottom: nil, left: nil, centerX: scrollView.centerXAnchor, padding: .init(top: 0, left: 0, bottom: 0, right: 0))
 
         let title = UILabel()
-        view.addSubview(title)
+        scrollView.addSubview(title)
         title.numberOfLines = 0
         title.text = "title : \(detailBook?.title ?? "")"
 
         let subtitle = UILabel()
-        view.addSubview(subtitle)
+        scrollView.addSubview(subtitle)
         subtitle.numberOfLines = 0
         subtitle.text = "subtitle : \(detailBook?.subtitle ?? "")"
 
         let authors = UILabel()
-        view.addSubview(authors)
+        scrollView.addSubview(authors)
         authors.numberOfLines = 0
         authors.text = "authors : \(detailBook?.authors ?? "")"
 
         let publisher = UILabel()
-        view.addSubview(publisher)
+        scrollView.addSubview(publisher)
         publisher.numberOfLines = 0
         publisher.text = "publisher : \(detailBook?.publisher ?? "")"
 
         let language = UILabel()
-        view.addSubview(language)
+        scrollView.addSubview(language)
         language.numberOfLines = 0
         language.text = "language : \(detailBook?.language ?? "")"
 
         let isbn10 = UILabel()
-        view.addSubview(isbn10)
+        scrollView.addSubview(isbn10)
         isbn10.numberOfLines = 0
         isbn10.text = "isbn10 : \(detailBook?.isbn10 ?? "")"
 
         let isbn13 = UILabel()
-        view.addSubview(isbn13)
+        scrollView.addSubview(isbn13)
         isbn13.numberOfLines = 0
         isbn13.text = "isbn13 : \(detailBook?.isbn13 ?? "")"
 
         let pages = UILabel()
-        view.addSubview(pages)
+        scrollView.addSubview(pages)
         pages.numberOfLines = 0
         pages.text = "pages : \(detailBook?.pages ?? "")"
 
         let year = UILabel()
-        view.addSubview(year)
+        scrollView.addSubview(year)
         year.numberOfLines = 0
         year.text = "year : \(detailBook?.year ?? "")"
 
         let rating = UILabel()
-        view.addSubview(rating)
+        scrollView.addSubview(rating)
         rating.numberOfLines = 0
         rating.text = "rating : \(detailBook?.rating ?? "")"
 
         let desc = UILabel()
-        view.addSubview(desc)
+        scrollView.addSubview(desc)
         desc.numberOfLines = 0
         desc.text = "desc : \(detailBook?.desc ?? "")"
 
         let url = UILabel()
-        view.addSubview(url)
+        scrollView.addSubview(url)
         url.numberOfLines = 0
         url.text = detailBook?.url
 
@@ -123,37 +119,37 @@ class DetailBookViewController: UIViewController {
                      padding: .init(top: 0, left: 16, bottom: 0, right: 16))
 
         subtitle.anchor(top: title.bottomAnchor, right: view.trailingAnchor, bottom: nil, left: view.leadingAnchor,
-                     padding: .init(top: 5, left: 16, bottom: 0, right: 16))
+                     padding: .init(top: 15, left: 16, bottom: 0, right: 16))
 
         authors.anchor(top: subtitle.bottomAnchor, right: view.trailingAnchor, bottom: nil, left: view.leadingAnchor,
-                     padding: .init(top: 5, left: 16, bottom: 0, right: 16))
+                     padding: .init(top: 15, left: 16, bottom: 0, right: 16))
 
         publisher.anchor(top: authors.bottomAnchor, right: view.trailingAnchor, bottom: nil, left: view.leadingAnchor,
-                     padding: .init(top: 5, left: 16, bottom: 0, right: 16))
+                     padding: .init(top: 15, left: 16, bottom: 0, right: 16))
 
         language.anchor(top: publisher.bottomAnchor, right: view.trailingAnchor, bottom: nil, left: view.leadingAnchor,
-                     padding: .init(top: 5, left: 16, bottom: 0, right: 16))
+                     padding: .init(top: 15, left: 16, bottom: 0, right: 16))
 
         isbn10.anchor(top: language.bottomAnchor, right: view.trailingAnchor, bottom: nil, left: view.leadingAnchor,
-                     padding: .init(top: 5, left: 16, bottom: 0, right: 16))
+                     padding: .init(top: 15, left: 16, bottom: 0, right: 16))
 
         isbn13.anchor(top: isbn10.bottomAnchor, right: view.trailingAnchor, bottom: nil, left: view.leadingAnchor,
-                     padding: .init(top: 5, left: 16, bottom: 0, right: 16))
+                     padding: .init(top: 15, left: 16, bottom: 0, right: 16))
 
         pages.anchor(top: isbn13.bottomAnchor, right: view.trailingAnchor, bottom: nil, left: view.leadingAnchor,
-                     padding: .init(top: 5, left: 16, bottom: 0, right: 16))
+                     padding: .init(top: 15, left: 16, bottom: 0, right: 16))
 
         year.anchor(top: pages.bottomAnchor, right: view.trailingAnchor, bottom: nil, left: view.leadingAnchor,
-                     padding: .init(top: 5, left: 16, bottom: 0, right: 16))
+                     padding: .init(top: 15, left: 16, bottom: 0, right: 16))
 
         rating.anchor(top: year.bottomAnchor, right: view.trailingAnchor, bottom: nil, left: view.leadingAnchor,
-                     padding: .init(top: 5, left: 16, bottom: 0, right: 16))
+                     padding: .init(top: 15, left: 16, bottom: 0, right: 16))
 
         desc.anchor(top: rating.bottomAnchor, right: view.trailingAnchor, bottom: nil, left: view.leadingAnchor,
-                     padding: .init(top: 5, left: 16, bottom: 0, right: 16))
+                     padding: .init(top: 15, left: 16, bottom: 0, right: 16))
 
-        url.anchor(top: desc.bottomAnchor, right: view.trailingAnchor, bottom: nil, left: view.leadingAnchor,
-                     padding: .init(top: 5, left: 16, bottom: 0, right: 16))
+        url.anchor(top: desc.bottomAnchor, right: view.trailingAnchor, bottom: scrollView.bottomAnchor, left: view.leadingAnchor,
+                     padding: .init(top: 15, left: 16, bottom: 0, right: 16))
 
     }
 }
